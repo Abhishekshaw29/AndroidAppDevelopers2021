c6ac07c12ac6d1d10ac86f7e4e1b5b1964b353d9
ABHISHEK SHAW <56201731+Abhishekshaw29@users.noreply.github.com>
2021-04-12 16:31:10 +0530

    final project for digilocker is ready

---
+++ app/src/main/java/com/abhishek/cardhaarapp/MainActivity.java
@@ -0,0 +1,93 @@
+package com.abhishek.cardhaarapp;
+import android.app.Activity;
+import android.app.DatePickerDialog;
+import android.os.Bundle;
+import android.view.View;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends Activity implements AdapterView.OnItemSelectedListener,View.OnClickListener {
+    EditText selectDate, selectTime;
+    private int mYear, mMonth, mDay, mHour, mMinute;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        selectDate = (EditText) findViewById(R.id.date);
+        selectTime = (EditText) findViewById(R.id.time);
+
+        selectDate.setOnClickListener(this);
+        selectTime.setOnClickListener(this);
+
+        // Spinner element
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+
+        // Spinner click listener
+        spinner.setOnItemSelectedListener(this);
+
+        // Spinner Drop down elements
+        List<String> categories = new ArrayList<String>();
+        categories.add("Select your Gender");
+        categories.add("Male");
+        categories.add("Female");
+
+        // Creating adapter for spinner
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, categories);
+
+        // Drop down layout style - list view with radio button
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        // attaching data adapter to spinner
+        spinner.setAdapter(dataAdapter);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        // On selecting a spinner item
+        String item = parent.getItemAtPosition(position).toString();
+
+        // Showing selected spinner item
+       // Toast.makeText(parent.getContext(), "Selected: " + item, Toast.LENGTH_LONG).show();
+    }
+
+    public void onNothingSelected(AdapterView<?> arg0) {
+        // TODO Auto-generated method stub
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        if (view == selectDate) {
+            final Calendar c = Calendar.getInstance();
+            mYear = c.get(Calendar.YEAR);
+            mMonth = c.get(Calendar.MONTH);
+            mDay = c.get(Calendar.DAY_OF_MONTH);
+
+
+            DatePickerDialog datePickerDialog = new DatePickerDialog(this,
+                    new DatePickerDialog.OnDateSetListener() {
+
+                        @Override
+                        public void onDateSet(DatePicker view, int year,
+                                              int monthOfYear, int dayOfMonth) {
+
+                            selectDate.setText(dayOfMonth + "-" + (monthOfYear + 1) + "-" + year);
+
+                        }
+                    }, mYear, mMonth, mDay);
+            datePickerDialog.show();
+        }
+
+    }
+}
